3ec1eff6f2f9552860b39c91a07079f35bc2ec3e
mahendrapawde <pawdemahendra13@gmail.com>
2024-11-29 11:42:29 +0530

    This is GitDemoProject7

---
+++ src/GitDemo7/Demo7.java
@@ -0,0 +1,23 @@
+package GitDemo7;
+
+public class Demo7 
+{
+public static void main(String[] args) 
+{
+System.out.println("This is My Git Demo Project 7");
+	
+}
+}
+
+//https://github.com/Mahendra-Pawde347/GitDemoProject2.git
+
+//echo "# GitDemoProject2" >> README.md
+//git init0
+//git add README.md
+//git commit -m "first commit"
+//git branch -M main
+//git remote add origin https://github.com/Mahendra-Pawde347/GitDemoProject2.git
+//git push -u origin main
+
+
+//
